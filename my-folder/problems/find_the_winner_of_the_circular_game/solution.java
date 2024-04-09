fb3093b3791c1368c884054651f86b60f7afe734
Vaibhavi Patel <pvaibhavi15@gmail.com>
2024-04-09 19:40:07 +0000

    Sync LeetCode submission - Find the Winner of the Circular Game (java)

--- my-folder/problems/find_the_winner_of_the_circular_game/solution.java
+++ my-folder/problems/find_the_winner_of_the_circular_game/solution.java
@@ -1,22 +1,14 @@
 class Solution {
     public int findTheWinner(int n, int k) {
 
-        //return find(n, k) + 1;
-
-        int winner = 0;
-
-        for(int i = 2; i <= n ; i++){
-            winner = (winner + k) % i;
-        }
-
-        return winner + 1;
+        return find(n,k) + 1;
+        
     }
 
-    private int find(int n , int k){
+    private int find(int n, int k){
         if(n == 1){
-            return 0;
+            return 0 ;
         }
-
-        return (find(n-1, k) + k) % n;
+        return (find(n-1,k) + k) % n;
     }
 }
