2f3f95ddb66bcc9039589230bd6a0f7f5ac8ad99
Vaibhavi Patel <pvaibhavi15@gmail.com>
2024-04-10 06:09:17 +0000

    Sync LeetCode submission - Sqrt(x) (java)

---
+++ my-folder/problems/sqrt(x)/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int mySqrt(int x) {
+        
+        if(x < 2){
+            return x;
+        }
+
+        int start = 2;
+        int end = x/2;
+
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            long num = (long) mid * mid;
+            if(num > x){
+                end = mid - 1;
+            }else if(num < x){
+                start = mid + 1;
+            }else{
+                return mid;
+            }
+        }
+
+        return end;
+        
+    }
+}
