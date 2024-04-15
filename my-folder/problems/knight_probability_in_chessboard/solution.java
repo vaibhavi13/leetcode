f1657841823199a25cb1d258b55a24c01db1ab74
Vaibhavi Patel <pvaibhavi15@gmail.com>
2024-04-15 18:10:45 +0000

    Sync LeetCode submission - Knight Probability in Chessboard (java)

--- my-folder/problems/knight_probability_in_chessboard/solution.java
+++ my-folder/problems/knight_probability_in_chessboard/solution.java
@@ -1,29 +1,27 @@
 class Solution {
     public double knightProbability(int n, int k, int row, int column) {
 
-        double dp[][][] = new double[k+1][n+1][n+1];
+        double dp[][][] = new double[k + 1][n + 1][n + 1];
+
         dp[0][row][column] = 1;
 
         int directions[][] = {{-2,1}, {-1,2}, {1,2}, {2,1}, {2,-1}, {1,-2}, {-1,-2}, {-2,-1}};
 
-
-        for(int i = 1 ; i <= k ; i++){
-            for(int j = 0 ; j < n ; j++){
-               for(int m = 0 ; m < n ; m++){
-                  for(int d[] : directions){
-                      int prev_row = j - d[0];
-                      int prev_col = m - d[1];
-
-                      if(prev_row >= 0 && prev_row < n && prev_col >= 0 && prev_col < n){
-                         dp[i][j][m] += dp[i-1][prev_row][prev_col]/8.0;
-                      }
-                  }
-               }
+        for(int m = 1 ; m <= k ; m++){
+            for(int i = 0; i < n ; i++){
+                for(int j = 0 ; j < n ; j++){
+                    for(int d[] : directions){
+                        int prev_i = i - d[0];
+                        int prev_j = j - d[1];
+                        if(prev_i >= 0 && prev_i < n && prev_j >=0 && prev_j < n){
+                            dp[m][i][j] += dp[m-1][prev_i][prev_j]/8.0;
+                        }
+                    }
+                }
             }
         }
-
-        double prob =0;
-
+        
+        double prob = 0;
         for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < n ; j++){
                 prob += dp[k][i][j];
@@ -31,10 +29,6 @@ class Solution {
         }
 
         return prob;
-
-
-
-
-
+        
     }
 }
