48bb7a562e2987ffdd23212c4bc6f9a20cc3b6b9
Vaibhavi Patel <pvaibhavi15@gmail.com>
2024-04-19 11:26:30 +0000

    Sync LeetCode submission - Rotting Oranges (java)

--- my-folder/problems/rotting_oranges/solution.java
+++ my-folder/problems/rotting_oranges/solution.java
@@ -1,26 +1,28 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
+        
         Queue<Pair<Integer,Integer>> queue = new LinkedList<>();
-
-        int freshOranges = 0;
+        int fresh = 0;
 
         for(int i = 0 ; i < grid.length ; i++){
             for(int j = 0 ; j < grid[i].length ; j++){
                 if(grid[i][j] == 1){
-                    freshOranges++;
+                    fresh++;
                 }else if(grid[i][j] == 2){
-                    queue.add(new Pair<>(i , j));
+                    queue.add(new Pair<>(i,j));
                 }
             }
         }
 
-        if(freshOranges == 0){
+        if(fresh == 0){
             return 0;
         }
-        queue.add(new Pair<>(-1, -1));
+
         int mins = -1;
 
-        int directions[][] = {{0,1}, {0 , -1}, {-1,0}, {1,0}};
+        int directions[][] = {{-1,0}, {1,0}, {0,-1}, {0,1}};
+        
+        queue.add(new Pair<>(-1,-1));
 
         while(queue.size() > 0){
 
@@ -41,21 +43,21 @@ class Solution {
                         int new_row = curr_row + d[0];
                         int new_col = curr_col + d[1];
 
-                        if(new_row >= 0 && new_row < grid.length && new_col >= 0 && new_col < grid[grid.length-1].length && grid[new_row][new_col] == 1){
+                        if(new_row >= 0 && new_row < grid.length && new_col >=0 && new_col < grid[grid.length-1].length && grid[new_row][new_col] == 1){
                             grid[new_row][new_col] = 2;
-                            freshOranges--;
-                            queue.add(new Pair<>(new_row,new_col));
+                            fresh--;
+                            queue.add(new Pair<>(new_row, new_col));
                         }
                     }
                 }
             }
 
             if(queue.size() > 0){
-               queue.add(new Pair<>(-1,-1));
+                queue.add(new Pair<>(-1,-1));
             }
 
         }
 
-        return freshOranges > 0 ? - 1 : mins;
+        return fresh > 0 ? -1 : mins;
     }
 }
