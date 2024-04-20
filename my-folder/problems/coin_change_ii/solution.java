c324fbecbe560472439cb8107880d97466bc8c28
Vaibhavi Patel <pvaibhavi15@gmail.com>
2024-04-20 22:34:43 +0000

    Sync LeetCode submission - Coin Change II (java)

---
+++ my-folder/problems/coin_change_ii/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+
+        int dp[][] = new int[coins.length + 1][amount + 1];
+
+        for(int i = 0 ; i < dp.length ; i++){
+            for(int j = 0 ; j < dp[i].length ; j++){
+                if(j == 0){
+                    dp[i][j] = 1;
+                }
+            }
+        }
+
+        for(int i = 1; i < dp.length ; i++){
+            for(int j =1 ; j < dp[i].length ; j++){
+                if(coins[i-1] <= j){
+                    dp[i][j] = dp[i][j - coins[i-1]] + dp[i-1][j];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[coins.length][amount];
+
+    }
+}
