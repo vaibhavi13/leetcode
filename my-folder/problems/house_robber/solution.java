cb85b9acc725804d4bd2c87f138c8700898dc24b
Vaibhavi Patel <pvaibhavi15@gmail.com>
2024-04-20 20:13:21 +0000

    Sync LeetCode submission - House Robber (java)

--- my-folder/problems/house_robber/solution.java
+++ my-folder/problems/house_robber/solution.java
@@ -1,25 +1,22 @@
 class Solution {
-    HashMap<Integer,Integer> map = new HashMap<>();
+
+    int dp[];
     public int rob(int[] nums) {
-        
-        if(nums.length == 1){
-            return nums[0];
-        }
-        
-        return Math.max(find(nums,0),find(nums,1));
+        dp = new int[nums.length + 1];
+        Arrays.fill(dp, - 1);
+
+        return Math.max(find(nums,0),find(nums,1)); 
     }
 
-    private int find(int[] nums, int index){
-        if(index >= nums.length){
+    private int find(int[] nums, int curr){
+        if(curr >= nums.length){
             return 0;
         }
 
-        if(map.containsKey(index)){
-            return map.get(index);
+        if(dp[curr] != -1){
+            return dp[curr];
         }
 
-        int ans = Math.max(nums[index] + find(nums, index + 2), find(nums, index + 1));
-        map.put(index, ans);
-        return ans;
+        return dp[curr] = Math.max(nums[curr] + find(nums, curr + 2), find(nums,curr+1));
     }
 }
