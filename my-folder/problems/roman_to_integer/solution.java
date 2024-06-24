b665276fdf19dee3770112db66f7de07c98f2344
Vaibhavi Patel <pvaibhavi15@gmail.com>
2024-06-24 19:15:57 +0000

    Sync LeetCode submission - Roman to Integer (java)

--- my-folder/problems/roman_to_integer/solution.java
+++ my-folder/problems/roman_to_integer/solution.java
@@ -3,39 +3,34 @@ class Solution {
 
         HashMap<String,Integer> map = new HashMap<>();
 
-        map.put("I", 1);
+        map.put("I",1);
         map.put("IV",4);
         map.put("V",5);
         map.put("IX",9);
         map.put("X",10);
-        map.put("XL",40);
         map.put("L",50);
+        map.put("C",100);
+        map.put("XL",40);
         map.put("XC",90);
         map.put("CD",400);
-        map.put("CM",900);
-        map.put("C",100);
         map.put("D",500);
+        map.put("CM",900);
         map.put("M",1000);
 
-        int n = 0;
-
-        int c = 0 ;
-
-        while(c < s.length()){
-            if(c+1 < s.length() && map.containsKey(s.substring(c,c+2))){
-                n += map.get(s.substring(c,c+2));
-                c += 2;
+        int ans = 0;
+        int i = 0;
+        while(i < s.length()){
+            String str = i < s.length() - 2 ? s.substring(i , i + 2) : s.substring(i);
+            if(map.containsKey(str)){
+              ans = ans + map.get(str);
+              i = i + 2; 
             }else{
-                n += map.get(s.substring(c,c+1));
-                c++;
-            }
+                ans = ans + map.get(String.valueOf(s.charAt(i)));
+                i++;
+            } 
         }
 
+        return ans;
 
-        return n;
-
-           
-
-        
     }
 }
