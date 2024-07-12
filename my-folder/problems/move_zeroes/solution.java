4d52c8bcc0796a1bdbe0eaa4a86915571b93fbe6
Vaibhavi Patel <pvaibhavi15@gmail.com>
2024-07-12 21:24:40 +0000

    Sync LeetCode submission - Move Zeroes (java)

---
+++ my-folder/problems/move_zeroes/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        
+        
+        int p = 0;
+        
+        for(int i = 0 ; i < nums.length ; i++){
+            if(nums[i] != 0){
+                nums[p++] = nums[i];
+            }
+        }
+        
+        for(int i = p ; i < nums.length ; i++){
+            nums[i] = 0;
+        }
+        
+        
+    }
+}
