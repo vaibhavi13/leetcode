edc83228c9986cf90c4473b58a397b7108ae7bef
Vaibhavi Patel <pvaibhavi15@gmail.com>
2024-07-17 03:21:40 +0000

    Sync LeetCode submission - Climbing Stairs (java)

--- my-folder/problems/climbing_stairs/solution.java
+++ my-folder/problems/climbing_stairs/solution.java
@@ -1,24 +1,27 @@
 class Solution {
     int dp[];
-
     public int climbStairs(int n) {
-        dp = new int[n + 1];
+        dp = new int[n+ 1];
+
         Arrays.fill(dp , -1);
-        return find(n,1) + find(n, 2);
+
+        return find(1, n) + find(2, n);
+        
     }
 
-    private int find(int n , int curr){
-        if(curr > n){
-            return 0;
-        }
+    private int find(int curr, int n){
         if(curr == n){
             return 1;
         }
-        
+
+        if(curr > n){
+            return 0;
+        }
+
         if(dp[curr] != -1){
             return dp[curr];
         }
 
-        return dp[curr] = find(n, curr + 1) + find(n, curr + 2);
+        return dp[curr] = find(curr + 1 , n) + find(curr + 2 , n);
     }
 }
