d332d95b0586fdd3c2cb4bb70d5aa993431b1286
Vaibhavi Patel <pvaibhavi15@gmail.com>
2024-07-18 02:16:01 +0000

    Sync LeetCode submission - Minimum Size Subarray Sum (java)

--- my-folder/problems/minimum_size_subarray_sum/solution.java
+++ my-folder/problems/minimum_size_subarray_sum/solution.java
@@ -1,23 +1,26 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
 
-        int minLen = Integer.MAX_VALUE;
+        int size = Integer.MAX_VALUE;
+
         int start = 0 ;
-        int end = 0;
-        int sum = 0;
+        int end = 0 ;
+        int sum = 0 ;
 
-        while(end < nums.length){
-            sum += nums[end];
+    
 
-            while(sum >= target && start <= end){
-                minLen = Math.min(minLen, end - start + 1);
-                sum -= nums[start];
-                start++;
-            }
-            end++;
+        while(start <= end && end < nums.length){
+              sum += nums[end];
+
+              while(sum >= target){
+                 size = Math.min(size, end - start + 1);
+                 sum -= nums[start];
+                 start++;
+              }
+              end++;
         }
 
-        return minLen == Integer.MAX_VALUE ? 0 : minLen;
+        return size == Integer.MAX_VALUE ? 0 : size;
         
     }
 }
